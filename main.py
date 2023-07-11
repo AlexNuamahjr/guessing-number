f1e4e84b0fb3d09947b67b6cfe57fbde94dee5d7
stephanie-opoku <opokuafriyiestephanie@gmail.com>
2023-07-11 05:16:47 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+i = 0
+while (i<=4):
+  guess_number = 9
+  user_input = int(input("enter guess a number(1-20) : "))
+  if user_input == guess_number:
+       print("you guess correct")
+  else:
+      print("guess is wrong")
+  i = i+ 1    
